cf382581b084d70a3be615631af5988c153a80d4
Andrew Mangal <amangal@ucsc.edu>
2022-11-24 20:48:49 -0800

    camera animation bug is fixed
    all that's left is the day-night cycle!

--- Assets/Scripts/ZoomCam.cs
+++ Assets/Scripts/ZoomCam.cs
@@ -7,21 +7,28 @@ public class ZoomCam : MonoBehaviour
 
     public Transform target;
     public Transform startPoint;
+    public Transform target2;
     public float step;
     public float m_FOV;
     public float zoomSpeed;
     public Camera cam;
     float speed;
 
-    private void onEnable()
+    public bool movingForward;
+
+    public void Awake()
     {
+        print("camera enabled");
         startPoint = this.transform;
         m_FOV = 60;
+        movingForward = true;
     }
+
     void Update()
     {
-        if (m_FOV <= 145.0f)
+        if (m_FOV <= 145.0f && movingForward)
         {
+            print("moving forward");
             speed = step * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
 
@@ -30,28 +37,35 @@ public class ZoomCam : MonoBehaviour
 
             transform.LookAt(target);
         }
+        else if (m_FOV > 145.0f)
+        {
+            movingForward = false;
+            print("start moving backwards");
+        }
+
+        if (m_FOV <= 60)
+        {
+            movingForward = true;
+            transform.position = startPoint.position;
+        }
+
+        if (movingForward == false)
+        {
+            print("now moving backwards");
+            speed = step * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target2.transform.position, speed);
+
+            m_FOV = m_FOV - (speed * zoomSpeed);
+            cam.fieldOfView = m_FOV;
+
+            //transform.LookAt(target2);
+        }
+        
+
 
 
 
         // Same as above, but setting the worldUp parameter to Vector3.left in this example turns the camera on its side
         // transform.LookAt(target, Vector3.left);
     }
-
-    // //movement speed in units per second
-    // public float movementSpeed = 10f;
-    // // public Vector3 pivotPoint = new Vector3(4f,4f,4f);
-
-    // void Update()
-    // {
-    //     //get the Input from Horizontal axis
-    //     float horizontalInput = Input.GetAxis("Horizontal");
-    //     //get the Input from Vertical axis
-    //     float verticalInput = Input.GetAxis("Vertical");
-        
-    //     //update the position
-    //     transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0,  verticalInput * movementSpeed * Time.deltaTime);
-    //     // Vector3 pivotPoint = new Vector3(0f,50f,0f);
-    //     // transform.RotateAround(pivotPoint, Vector3.up, 90*Time.deltaTime);
-
-    // }
 }
